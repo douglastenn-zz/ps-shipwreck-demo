aa8573a2d1d55bb3e26eeb0bd0bc1a2a56fecb90
Douglas Tenório <douglas.develop@gmail.com>
2017-08-08 01:32:21 -0300

    First commit

---
+++ src/main/java/com/boot/dasboot/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.boot.dasboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home() {
+        return "Das Boot, reporting for duty!";
+    }
+}
